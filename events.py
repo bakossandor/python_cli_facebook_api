f50f0c1d8dd27b9e11ed1ab01aa20ebf82adfe4d
Sándor Bakos <bakos.webd@gmail.com>
2019-01-20 10:27:38 +0100

    managed to solve the pagination

--- events.py
+++ events.py
@@ -4,11 +4,22 @@ def read_token():
     f = open("token.txt")
     return f.read()
 
-
-URL = "https://graph.facebook.com/"
-QUERY="me"
-FIELDS = "?fields=id,name"
+ORIGIN = "https://graph.facebook.com/"
 TOKEN = f"&access_token={read_token()}"
 
-r = requests.get(f"{URL}{QUERY}{FIELDS}{TOKEN}")
-print(str(r.text))
+def search(location):
+    QUERY = f"search?type=place&center={'56.151564, 10.201814'},distance=100&fields=about,name,fun_count,rating_count,category_list"
+    URL = f"{ORIGIN}{QUERY}{TOKEN}"
+    data_set = []
+    return get_and_return(URL, data_set)
+    
+def get_and_return(url, data_set):
+    r = requests.get(url).json()
+    for place in r["data"]:
+        data_set.append(place)
+    if "next" in r["paging"]:
+        get_and_return(r["paging"]["next"], data_set)
+    return data_set
+
+
+print(search("Aarhus"))
